b31c903351cea744747a6f7abf38b9e2c0d6541b
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-03-25 21:16:33 -0700

    this is second time commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Mobile Bazer</h1>
+            <p>Find your favorite mobile</p>
+        </div>
+    );
+};
+
+export default Header;
